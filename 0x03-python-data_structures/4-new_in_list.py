db1bf3ef2698dc14eaa5710dca26d530d54cf398
UbaidaNA <ubnapari@gmail.com>
2023-06-12 23:13:07 +0000

    Copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
-    dup = [x for x in my_list]
-    dup[idx] = element
-    return (dup)
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
